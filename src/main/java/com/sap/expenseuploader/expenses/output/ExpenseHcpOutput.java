317a57124a326f6479fd6b41f98cdea67b70eb96
Fabian Flechtmann <fabian.flechtmann@sap.com>
2016-09-26 16:56:22 +0200

    Merged 'Dump json payloads to files'

--- src/main/java/com/sap/expenseuploader/expenses/output/ExpenseHcpOutput.java
+++ src/main/java/com/sap/expenseuploader/expenses/output/ExpenseHcpOutput.java
@@ -15,8 +15,16 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import javax.management.relation.RoleNotFoundException;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,9 +34,10 @@ import static com.sap.expenseuploader.config.HcpConfig.getBodyFromResponse;
  * This is the most common method of output, to upload expenses to the HCP realspend
  * backend.
  */
-public class ExpenseHcpOutput implements ExpenseOutput
-{
+public class ExpenseHcpOutput implements ExpenseOutput {
     private final Logger logger = LogManager.getLogger(this.getClass());
+
+    public static final Path REQ_DUMP_FOLDER = Paths.get("requests");
 
     private HcpConfig hcpConfig;
     private CostCenterConfig costCenterConfig;
@@ -40,35 +49,55 @@ public class ExpenseHcpOutput implements ExpenseOutput
     }
 
     @Override
-    public void putExpenses( List<Expense> expenses )
-    {
+    public void putExpenses(List<Expense> expenses) {
         logger.info("Writing expenses to HCP at " + this.hcpConfig.getHcpUrl());
+
+        deleteRequestFolder();
 
         try {
             // Upload
-            for( String user : this.costCenterConfig.getCostCenterUserList() ) {
+            for (String user : this.costCenterConfig.getCostCenterUserList()) {
                 List<String> costCenters = this.costCenterConfig.getCostCenters(user);
                 List<Expense> userExpenses = new ArrayList<>();
-                for( Expense expense : expenses ) {
-                    if( expense.isInCostCenter(costCenters) ) {
+                for (Expense expense : expenses) {
+                    if (expense.isInCostCenter(costCenters)) {
                         userExpenses.add(expense);
                     }
                 }
-                if( userExpenses.isEmpty() ) {
+                if (userExpenses.isEmpty()) {
                     logger.info("No expenses to put for user " + user);
                     continue;
                 }
                 uploadExpenses(userExpenses, user);
             }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to post expenses", e);
         }
-        catch( Exception e ) {
-            logger.error(e);
+    }
+
+    private void deleteRequestFolder() {
+        try {
+            logger.info("Deleting old request folder");
+            Files.walkFileTree(REQ_DUMP_FOLDER, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }catch (Exception e) {
+            logger.error("Failed to delete " + REQ_DUMP_FOLDER, e);
         }
     }
 
     private long getExpenseCount()
-        throws URISyntaxException, IOException, ParseException
-    {
+            throws URISyntaxException, IOException, ParseException {
         // TODO this does not show the expenses of other users, so on-behalf postings can not be checked.
 
         URIBuilder uriBuilder = new URIBuilder(this.hcpConfig.getHcpUrl() + "/rest/expense/count");
@@ -77,7 +106,7 @@ public class ExpenseHcpOutput implements ExpenseOutput
 
         // Check response
         int statusCode = response.getStatusLine().getStatusCode();
-        if( statusCode == 200 ) {
+        if (statusCode == 200) {
             // Parse JSON
             String responseAsString = getBodyFromResponse(response);
             JSONParser parser = new JSONParser();
@@ -85,8 +114,8 @@ public class ExpenseHcpOutput implements ExpenseOutput
             return (Long) propertyMap.get("count");
         } else {
             logger.error(String.format("Got http code %s while uploading %s expenses for user %s",
-                statusCode,
-                this.hcpConfig.getHcpUser()));
+                    statusCode,
+                    this.hcpConfig.getHcpUser()));
             logger.error("URL was: " + uriBuilder.build());
             logger.error("Error is: " + getBodyFromResponse(response));
             throw new IOException("Unable to get count of expenses");
@@ -94,11 +123,12 @@ public class ExpenseHcpOutput implements ExpenseOutput
     }
 
     private void uploadExpenses( List<Expense> expenses, String user )
-            throws URISyntaxException, IOException, RoleNotFoundException {
+        throws URISyntaxException, IOException, RoleNotFoundException
+    {
         // Create JSON payload
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         JsonArray expensesAsJson = (JsonArray) gson.toJsonTree(expenses);
-        for( JsonElement e : expensesAsJson ) {
+        for (JsonElement e : expensesAsJson) {
             // Add approver to each expense
             JsonObject expenseAsJson = (JsonObject) e;
             expenseAsJson.addProperty("approver", this.hcpConfig.getHcpUser());
@@ -106,7 +136,7 @@ public class ExpenseHcpOutput implements ExpenseOutput
         JsonObject payload = new JsonObject();
         payload.add("expenses", expensesAsJson);
         payload.addProperty("user", user);
-        logger.debug(new GsonBuilder().setPrettyPrinting().create().toJson(payload));
+        dumpRequest(user, new GsonBuilder().setPrettyPrinting().create().toJson(payload));
 
         // TODO delta merge: Compare expenses with what's already there
         // Blocked until we have on-behalf lookup
@@ -120,20 +150,40 @@ public class ExpenseHcpOutput implements ExpenseOutput
 
         logger.info(String.format("Posting %s expenses for user %s", expenses.size(), user));
 
+        final long start = System.currentTimeMillis();
         HttpResponse response = this.hcpConfig.withOptionalProxy(request).execute().returnResponse();
+        final long duration = System.currentTimeMillis() - start;
 
         // Check response
         int statusCode = response.getStatusLine().getStatusCode();
-        if( statusCode == 200 ) {
-            logger.info(String.format("Successfully uploaded %s expenses for user %s", expenses.size(), user));
+        if (statusCode == 200) {
+            logger.info(String.format("Successfully uploaded %s expenses for user %s (took %s sec)", expenses.size(), user, duration / 1000));
         } else {
             logger.error(String.format("Got http code %s while uploading %s expenses for user %s",
-                statusCode,
-                expenses.size(),
-                user));
+                    statusCode,
+                    expenses.size(),
+                    user));
             logger.error("URL was: " + uriBuilder.build());
             logger.error("Error is: " + getBodyFromResponse(response));
             System.exit(1);
+        }
+    }
+
+    private void dumpRequest(final String key, final String s) {
+        try {
+            if (!Files.isDirectory(REQ_DUMP_FOLDER)) {
+                Files.createDirectory(REQ_DUMP_FOLDER);
+            }
+        } catch(Exception e) {
+            logger.error("Failed to create " + REQ_DUMP_FOLDER);
+            return;
+        }
+
+        final File file = new File(REQ_DUMP_FOLDER.toFile(), key + ".json");
+        try (PrintWriter writer = new PrintWriter(file)) {
+            writer.write(s);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
